b60e2e52d8883d7a6f31514657a3286b7767d1cf
kshramt <thisisdummy@example.com>
2013-12-22 02:47:59 +0900

    Add `Juke.new_dsl`

---
+++ src/Juke.jl
@@ -0,0 +1,70 @@
+module Juke
+
+export new_dsl
+
+macro p(ex)
+    :(println($(string(ex)), ":\n", $ex, "\n"))
+end
+
+JobName = Union(String, Symbol)
+
+type Job
+    command::Function
+    name::JobName
+    done::Bool
+end
+Job(command, name) = Job(command, name, false)
+Job(name) = Job((_)->nothing, name)
+
+type JobInfo
+    name::JobName
+    deps::Set{JobName}
+end
+
+function new_dsl()
+    # Environment
+    name_graph = Dict{JobName, Set{JobName}}()
+    name_to_job = Dict{JobName, Job}()
+
+    # DSL
+    function job(command::Function, name::JobName, deps)
+        name_to_job[name] = Job(command, name)
+        name_graph[name] = Set{JobName}(deps...)
+    end
+    job(command::Function, name) = job(command, name, [])
+    job(name::JobName, deps) = job((_)->nothing, name, deps)
+    job(name) = job(name, [])
+
+    function invoke(target::JobName)
+        target_job = get_job(target)
+        target_job.done = true
+
+        deps = get_deps(target)
+        for dep in deps
+            if !get_job(dep).done
+                invoke(dep)
+            end
+        end
+
+        target_job.command(JobInfo(target, deps))
+    end
+    invoke() = invoke(:default)
+
+    # Helper
+    function make_get_set!(d, inifn)
+        (k)->begin
+            if haskey(d, k)
+                d[k]
+            else
+                d[k] = inifn(k)
+            end
+        end
+    end
+    get_job = make_get_set!(name_to_job, (name)->Job(name))
+    get_deps = make_get_set!(name_graph, (_)->Set{JobName}())
+
+    # Export
+    job, invoke
+end
+
+end
